de942eef12cd93c9c6ac0710207c0c62323a89c6
DaviRain-Su <davirain.yin@gamil.com>
2021-03-02 16:26:27 +0800

    update ...

--- erc20/lib.rs
+++ erc20/lib.rs
@@ -2,30 +2,39 @@
 //! 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use ink_env::{Environment};
+use ink_env::{Environment, AccountId};
 use ink_lang as ink;
 
 
-// type Balance = <ink_env::DefaultEnvironment as Environment>::Balance;
+type Balance = <ink_env::DefaultEnvironment as Environment>::Balance;
 
 #[ink::chain_extension]
 pub trait FetchErc20 {
     type ErrorCode = Erc20Error;
 
-    // #[ink(extension = 1101, returns_result = false)]
-    // fn transfer(to: AccountId, value: Balance);
-    //
-    // #[ink(extension = 1102, returns_result = false)]
-    // fn transfer_from(from: AccountId, to: AccountId, value: Balance);
-    //
-    // #[ink(extension = 1103, returns_result = false)]
-    // fn transfer_help(from: AccountId, to: AccountId, value: Balance);
-    //
-    // #[ink(extension = 1104, returns_result = false)]
-    // fn allowance(to: AccountId, value: Balance);
-
     #[ink(extension = 1101, returns_result = false)]
     fn fetch_random() -> [u8; 32];
+
+    #[ink(extension = 1102, returns_result = false)]
+    fn create(owner: AccountId, total_supply: Balance);
+
+    #[ink(extension = 1103, returns_result = false)]
+    fn total_supply() -> Balance;
+
+    #[ink(extension = 1104, returns_result = false)]
+    fn balance_of(account: AccountId) -> Balance;
+
+    #[ink(extension = 1105, returns_result = false)]
+    fn allowance_of(owner: AccountId, spender: AccountId) -> Balance;
+
+    #[ink(extension = 1106, returns_result = false)]
+    fn transfer_from(from: AccountId, to: AccountId, value: Balance);
+
+    #[ink(extension = 1107, returns_result = false)]
+    fn transfer_help(from: AccountId, to: AccountId, value: Balance);
+
+    #[ink(extension = 1108, returns_result = false)]
+    fn allowance(to: AccountId, value: Balance);
 }
 
 
@@ -72,38 +81,38 @@ mod erc20 {
 
     use super::Erc20Error;
 
-    use ink_storage::collections::HashMap as StorageHashMap;
+    // use ink_storage::collections::HashMap as StorageHashMap;
 
-    // 定义的存储类型
+    // // 定义的存储类型
     #[ink(storage)]
     pub struct Erc20 {
-        // 总的供给量
-        total_supply: Balance,
-        // 用户对应的供给量
-        balances: StorageHashMap<AccountId, Balance>,
-        // 允许一个账户向一个账户转账的金额
-        allowance: StorageHashMap<(AccountId, AccountId), Balance>,
-        // randoms
+    //     // 总的供给量
+    //     total_supply: Balance,
+    //     // 用户对应的供给量
+    //     balances: StorageHashMap<AccountId, Balance>,
+    //     // 允许一个账户向一个账户转账的金额
+    //     allowance: StorageHashMap<(AccountId, AccountId), Balance>,
+    //     // randoms
         value: [u8; 32],
     }
 
-    // 定义的事件类型
-    #[ink(event)]
-    pub struct Transfer {
-        #[ink(topic)]
-        from: Option<AccountId>,
-        #[ink(topic)]
-        to: Option<AccountId>,
-        value: Balance,
-    }
-    #[ink(event)]
-    pub struct Approve {
-        #[ink(topic)]
-        owner: AccountId,
-        #[ink(topic)]
-        spender: AccountId,
-        value: Balance,
-    }
+    // // 定义的事件类型
+    // #[ink(event)]
+    // pub struct Transfer {
+    //     #[ink(topic)]
+    //     from: Option<AccountId>,
+    //     #[ink(topic)]
+    //     to: Option<AccountId>,
+    //     value: Balance,
+    // }
+    // #[ink(event)]
+    // pub struct Approve {
+    //     #[ink(topic)]
+    //     owner: AccountId,
+    //     #[ink(topic)]
+    //     spender: AccountId,
+    //     value: Balance,
+    // }
 
     // for random
     #[ink(event)]
@@ -119,45 +128,57 @@ mod erc20 {
         // 定义的构造函数
         #[ink(constructor)]
         pub fn new(total_supply: Balance) -> Self {
-
-            // 获得合约调用者AccountId
+            //
+            // // 获得合约调用者AccountId
             let caller = Self::env().caller();
-            
-            // 在创建时讲总供给量默认与合约的创建者做绑定
-            let mut balances = StorageHashMap::<AccountId, Balance>::new();
-            balances.insert(caller, total_supply);
-
-            // 合约创建时的调用的事件
-            Self::env().emit_event(Transfer {
-                from: None,
-                to: Some(caller),
-                value: total_supply,
-            });
+            //
+            // // 在创建时讲总供给量默认与合约的创建者做绑定
+            // let mut balances = StorageHashMap::<AccountId, Balance>::new();
+            // balances.insert(caller, total_supply);
+            //
+            // // 合约创建时的调用的事件
+            // Self::env().emit_event(Transfer {
+            //     from: None,
+            //     to: Some(caller),
+            //     value: total_supply,
+            // });
+            //
+            // Self {
+            //     total_supply,
+            //     balances,
+            //     allowance: StorageHashMap::new(),
+            //     value: Default::default(),
+            // }
+            let _ = Self::env().extension().create(caller, total_supply);
 
             Self {
-                total_supply,
-                balances,
-                allowance: StorageHashMap::new(),
                 value: Default::default(),
             }
         }
 
         // 定义的获得总供给量的函数
         #[ink(message)]
-        pub fn total_supply(&self) -> Balance {
-            self.total_supply
+        pub fn total_supply(&self) -> Result<Balance> {
+            // self.total_supply
+            let balance = Self::env().extension().total_supply()?;
+            Ok(balance)
         }
+
 
         // 定义的获得某个账户有多少余额的函数
         #[ink(message)]
-        pub fn balance_of(&self, account: AccountId) -> Balance {
-            *self.balances.get(&account).unwrap_or(&0)
+        pub fn balance_of(&self, account: AccountId) -> Result<Balance> {
+            // *self.balances.get(&account).unwrap_or(&0)
+            let balance = Self::env().extension().balance_of(account)?;
+            Ok(balance)
         }
 
         // 定义的获得owner可以向spend转账的余额
         #[ink(message)]
-        pub fn allowance_of(&self, owner: AccountId, spender: AccountId) -> Balance {
-            *self.allowance.get(&(owner, spender)).unwrap_or(&0)
+        pub fn allowance_of(&self, owner: AccountId, spender: AccountId) -> Result<Balance> {
+            // *self.allowance.get(&(owner, spender)).unwrap_or(&0)
+            let balance = Self::env().extension().allowance_of(owner, spender)?;
+            Ok(balance)
         }
 
         // 定义的合约可以向账户转账的函数
@@ -173,24 +194,27 @@ mod erc20 {
         // 定义的 更新allowance的函数
         #[ink(message)]
         pub fn allowance(&mut self, spender: AccountId, value: Balance) -> Result<()> {
-            let caller = self.env().caller();
+            // let caller = self.env().caller();
+            //
+            // let caller_balance = self.balance_of(caller.clone());
+            //
+            // // 确保caller的持有的value数量一定大于spender持有的value数量的
+            // if caller_balance < value {
+            //     return Err(Erc20Error::InSufficientBalance);
+            // }
+            //
+            // // 插入 ：拥有者可以向花费者转移的value
+            // self.allowance.insert((caller, spender), value);
+            //
+            // // 发送事件
+            // self.env().emit_event(Approve {
+            //     owner: caller,
+            //     spender: spender,
+            //     value: value,
+            // });
 
-            let caller_balance = self.balance_of(caller.clone());
+            let _  = Self::env().extension().allowance(spender, value);
 
-            // 确保caller的持有的value数量一定大于spender持有的value数量的
-            if caller_balance < value {
-                return Err(Erc20Error::InSufficientBalance);
-            }
-
-            // 插入 ：拥有者可以向花费者转移的value
-            self.allowance.insert((caller, spender), value);
-
-            // 发送事件
-            self.env().emit_event(Approve {
-                owner: caller,
-                spender: spender,
-                value: value,
-            });
             Ok(())
         }
 
@@ -203,18 +227,19 @@ mod erc20 {
             to: AccountId,
             value: Balance,
         ) -> Result<()> {
-
-            //先做判断，判断是不是有足够的余额可以从from到to的余额转账
-            let allowance = self.allowance_of(from, to);
-            // 如果查到余额不足，抛出错误
-            if allowance < value {
-                return Err(Erc20Error::InSufficientAllowence);
-            }
-
-            // 更新allowance函数
-            self.allowance.insert((from, to), allowance - value);
-
-            self.transfer_helper(from, to, value)?;
+            //
+            // //先做判断，判断是不是有足够的余额可以从from到to的余额转账
+            // let allowance = self.allowance_of(from, to);
+            // // 如果查到余额不足，抛出错误
+            // if allowance < value {
+            //     return Err(Erc20Error::InSufficientAllowence);
+            // }
+            //
+            // // 更新allowance函数
+            // self.allowance.insert((from, to), allowance - value);
+            //
+            // self.transfer_helper(from, to, value)?;
+            let _ = Self::env().extension().transfer_from(from, to, value);
 
             Ok(())
         }
@@ -227,26 +252,27 @@ mod erc20 {
             value: Balance,
         ) -> Result<()> {
 
-            // 判断from是不是有足够的余额可以向to转账
-            let from_balance = self.balance_of(from.clone());
-            if from_balance < value {
-                return Err(Erc20Error::InSufficientBalance);
-            }
+            // // 判断from是不是有足够的余额可以向to转账
+            // let from_balance = self.balance_of(from.clone());
+            // if from_balance < value {
+            //     return Err(Erc20Error::InSufficientBalance);
+            // }
+            //
+            // // 更新from账户对应的balance余额
+            // self.balances.insert(from, from_balance - value);
+            //
+            // let to_balance = self.balance_of(to);
+            // // 更新to账户对应的balance余额
+            // self.balances.insert(to, to_balance + value);
+            //
+            // // 发送事件
+            // self.env().emit_event(Transfer {
+            //     from: Some(from),
+            //     to: Some(to),
+            //     value: value,
+            // });
 
-            // 更新from账户对应的balance余额
-            self.balances.insert(from, from_balance - value);
-
-            let to_balance = self.balance_of(to);
-            // 更新to账户对应的balance余额
-            self.balances.insert(to, to_balance + value);
-
-            // 发送事件
-            self.env().emit_event(Transfer {
-                from: Some(from),
-                to: Some(to),
-                value: value,
-            });
-
+            let _ = Self::env().extension().transfer_help(from, to, value);
 
             Ok(())
         }
