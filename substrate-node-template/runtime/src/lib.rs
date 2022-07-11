3934396fc8c58e25ef6c1474448c52cbae7edc8d
Davirain <davirain.yin@gmail.com>
2022-07-11 19:35:10 +0800

    add pallet-ibc

--- substrate-node-template/runtime/src/lib.rs
+++ substrate-node-template/runtime/src/lib.rs
@@ -266,6 +266,12 @@ impl pallet_template::Config for Runtime {
 	type Event = Event;
 }
 
+
+/// Configure the pallet-template in pallets/template.
+impl pallet_ibc::Config for Runtime {
+	type Event = Event;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -283,6 +289,7 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		Ibc: pallet_ibc,
 	}
 );
 
